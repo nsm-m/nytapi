c79d815af4e012ee7d85297837a5bd9ad5f1d4be
nsm-m <nasma.dev@gmail.com>
2022-09-28 15:37:30 -0700

    env variables set

--- functions/fetch-weather/fetch-weather.js
+++ functions/fetch-weather/fetch-weather.js
@@ -1,24 +1,11 @@
 
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const axios = require('axios');
-// require('dotenv').config()
-// console.log(process.env)
 
-
-
-
-
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   const { long, lat } = event.queryStringParameters;
-
-  const API_SECRET_KEY = "8bf1e6357a7a55a3d9796e4e906bc623";
-  const API_SECRET = process.env.API_SECRET;
-  const test1 = JSONAPI_SECRET
-
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_SECRET_KEY}&units=metric`;
-  //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_SECRET}&units=metric`;
+  const weatherKey = process.env.WEATHER_SECRET;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${weatherKey}&units=metric`;
 
 
   try {
